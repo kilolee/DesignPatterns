0f8ace75db41c281221777be46043f92825fe52c
kilo <642026443@qq.com>
2018-03-11 21:35:17 +0800

    观察者模式

---
+++ ObserverPattern/src/com/kilo/fromJava/observer/ConcreteWeatherObserver.java
@@ -0,0 +1,31 @@
+package com.kilo.fromJava.observer;
+
+import com.kilo.fromJava.subject.ConcreteWeatherSubject;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * 具体的观察者对象
+ * Created by kilo on 2018/3/11.
+ */
+public class ConcreteWeatherObserver implements Observer {
+    //观察者的名称
+    private String observerName;
+
+    public String getObserverName() {
+        return observerName;
+    }
+
+    public void setObserverName(String observerName) {
+        this.observerName = observerName;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        //第一种是推的方式
+        System.out.println(observerName + "收到了消息，目标推送过来的是" + arg);
+        //第二种是拉的方式
+        System.out.println(observerName + "收到了消息，主动到目标对象中去拉，拉的内容是"+((ConcreteWeatherSubject)o).getContent() );
+    }
+}
