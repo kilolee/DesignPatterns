2536350e786cddb6299bf20115205fe2ef4cd496
knightlylee <642026443@qq.com>
2018-08-15 09:37:58 +0800

    原型模式

--- Prototype/src/Main.java
+++ Prototype/src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         manager.register("warning box", mbox);
         manager.register("slash box", sbox);
 
-        //生成
+        //克隆
         Product p1 = manager.create("strong message");
         p1.use("Hello, world.");
         Product p2 = manager.create("warning box");
