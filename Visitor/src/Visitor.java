7413b7b052e596306f573df1ba594ea120876051
knightlylee <642026443@qq.com>
2018-08-17 22:05:17 +0800

    Visitor模式

--- Visitor/src/Visitor.java
+++ Visitor/src/Visitor.java
@@ -1,6 +1,7 @@
 /**
  * Created by kilo on 2018/8/17.
  * 表示访问者的抽象类，可以访问文件和文件夹
+ * 对被访问对象(文件、文件夹)的处理都集中在访问者(visitor)中
  */
 public abstract class Visitor {
     public abstract void visit(File file);
